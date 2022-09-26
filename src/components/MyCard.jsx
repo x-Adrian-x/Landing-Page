ea4daed40b01d2813d7e5a594f28d3f21cc78a6c
x-Adrian-x <adrianesteban98@hotmail.com>
2022-09-25 22:30:42 -0500

    correccion de imagenes

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,7 +1,7 @@
-export const MyCard = ({ url, titulo, descripcion, url_preview, url_github }) => {
+export const MyCard = ({ titulo, descripcion, url_preview, url_github }) => {
   return (
     <div className="m-4 bg-white px-20 py-12 rounded-2xl border-8 border-slate-700 shadow-2xl shadow-slate-700 md:my-36 my-10">
-      <img src={url} className="rounded-2xl mx-auto mb-4 shadow-2xl" />
+      <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" className="rounded-2xl mx-auto mb-4 shadow-2xl" />
       <h1 className="font-bold text-2xl mb-4">{titulo}</h1>
       <h1 className="mb-6">{descripcion}</h1>
       <a href={url_preview} target="_blank" className="bg-green-700 mr-2 text-white px-6 py-2 rounded-full">
